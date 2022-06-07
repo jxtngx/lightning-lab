c255148a48dd3db3246a8aff1c826ec2660e8a0f
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-06-07 17:19:06 -0400

    update description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name="lightning-agents",
     version="0.0.1",
-    description="for local, editable installs of the directory learning_agents",
+    description="for local, editable installs of the lightning_agents module",
     url="https://github.com/JustinGoheen/lightning-pod",
     author="Justin Goheen",
     license="Apache 2.0",
