3b144864cf77ab5cd6a2b03f3b39584360c8515a
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-06-25 13:08:10 -0400

    remove unneeded list

--- lightning_pod/cli/pod.py
+++ lightning_pod/cli/pod.py
@@ -38,6 +38,6 @@ def run_trainer(hydra_args):
     trainer = os.path.join("lightning_pod", "agents", "trainer.py")
     hydra_args = list(hydra_args)
     hydra_args = [f"'trainer.{i}'" for i in hydra_args]
-    hydra_args = " ".join(list(hydra_args))
+    hydra_args = " ".join(hydra_args)
     run_command = "".join(["python", " ", trainer, " ", hydra_args])
     os.system(run_command)
