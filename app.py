5912c4aeb5f88bd86b3c810bf26db123c39098e6
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-06-28 23:13:43 -0400

    implement Data dataclass

--- app.py
+++ app.py
@@ -6,8 +6,22 @@ import dash_bootstrap_components as dbc
 
 from dash import html
 from dash.dependencies import Input, Output
-from components.ui import create_figure
+from components.ui import create_figure, find_index
 from components.ui import NavBar, Body
+
+from dataclasses import dataclass
+from pathlib import Path
+from torch.utils.data import TensorDataset
+
+
+@dataclass
+class Data:
+    """class for storing ground truth and prediction torch.TensorDatasets"""
+
+    predictions_fname: Path = os.path.join("data", "predictions", "predictions.pt")
+    predictions: TensorDataset = torch.load(predictions_fname)
+    ground_truths_fname: Path = os.path.join("data", "training_split", "val.pt")
+    ground_truths: TensorDataset = torch.load(ground_truths_fname)
 
 
 class DashWorker(L.LightningWork):
@@ -25,21 +39,13 @@ class DashWorker(L.LightningWork):
             [Output("left-fig", "figure"), Output("right-fig", "figure")],
             [Input("dropdown", "value")],
         )
-        def update_figure(digit_value):
-            predictions_fname = os.path.join("data", "predictions", "predictions.pt")
-            predictions = torch.load(predictions_fname)
-            ground_truths_fname = os.path.join("data", "training_split", "val.pt")
-            ground_truths = torch.load(ground_truths_fname)
-
-            for i in range(len(ground_truths)):
-                if ground_truths[i][1] == digit_value:
-                    sample_idx = i
-                    break
-
-            ground_truth_fig = create_figure(
-                ground_truths[sample_idx][0], "Ground Truth"
-            )
-            prediction_fig = create_figure(predictions[sample_idx][0], "Decoded")
+        def update_figure(label_value):
+            xidx = 0
+            idx = find_index(Data.ground_truths, label=label_value, label_idx=1)
+            gt = Data.ground_truths[idx][xidx]
+            pred = Data.predictions[idx][xidx]
+            ground_truth_fig = create_figure(gt, "Ground Truth")
+            prediction_fig = create_figure(pred, "Decoded")
             return ground_truth_fig, prediction_fig
 
         app.run_server(host=self.host, port=self.port)
