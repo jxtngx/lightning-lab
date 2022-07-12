005e6cea064e465cf9ba361778f37c5dce0dd1d4
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-07-11 21:53:14 -0400

    update pipeline

--- lightning_pod/pipeline/dataset.py
+++ lightning_pod/pipeline/dataset.py
@@ -1,6 +1,8 @@
+from typing import Any
+
 from torchvision.datasets import MNIST
 
 
 class LitDataset(MNIST):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
