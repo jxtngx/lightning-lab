49166fe60998b0b36a75ee91c046c26c91f5486c
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-08-06 19:25:38 -0400

    update

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+# Copyright Justin R. Goheen.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from pathlib import Path
 
 from setuptools import find_packages, setup
@@ -12,7 +27,7 @@ long_description = (rootdir / "README.md").read_text()
 
 setup(
     name="lightning-pod",
-    version="0.0.4.1",
+    version="0.0.5",
     description="A Lightning.ai application seed",
     long_description=long_description,
     long_description_content_type="text/markdown",
