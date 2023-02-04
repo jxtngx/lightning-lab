0e7962d8ad6f7d72ca2d0a5eeac97f64427fb7e6
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-02-04 11:52:36 -0500

    update

--- reactui/src/App.tsx
+++ reactui/src/App.tsx
@@ -1,45 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Container, Grid } from '@mui/material';
+import { MetricOne } from "./components/metricOne";
+import {MetricTwo} from "./components/metricTwo";
+import {MetricThree} from "./components/metricThree";
+import {MetricFour} from "./components/metricFour";
 
-import { Button } from "@mui/material"
+const Page = () => (
+    <Container maxWidth={false}>
+        <Grid
+        container
+        spacing={3}
+        >
+            <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+            >
+            <MetricOne />
+            </Grid>
+            <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+            >
+            <MetricTwo />
+            </Grid>
+            <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+            >
+            <MetricThree />
+            </Grid>
+            <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+            >
+            <MetricFour />
+            </Grid>
+        </Grid>
+    </Container>
+);
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-    {/* <select>
-        <option value="grapefruit">Grapefruit</option>
-        <option value="lime">Lime</option>
-        <option selected value="coconut">Coconut</option>
-        <option value="mango">Mango</option>
-    </select> */}
-    </div>
-    </div>
-  )
-}
-
-
-export default App;
+export default Page;
