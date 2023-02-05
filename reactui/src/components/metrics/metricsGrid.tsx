b8dbef6dd31882258f4fe6ef76b09baefd888558
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-02-04 22:22:09 -0500

    apply prettier

--- reactui/src/components/metrics/metricsGrid.tsx
+++ reactui/src/components/metrics/metricsGrid.tsx
@@ -6,25 +6,25 @@ import { MetricThree } from "./metricThree";
 import { MetricFour } from "./metricFour";
 
 export const MetricsGrid = () => (
-  <Container maxWidth={false}>
-    {/* Metric Cards */}
-    <Grid container spacing={3}>
-      {/* Metric One */}
-      <Grid item lg={3} sm={6} xl={3} xs={12}>
-        <MetricOne />
-      </Grid>
-      {/* Metric Two */}
-      <Grid item xl={3} lg={3} sm={6} xs={12}>
-        <MetricTwo />
-      </Grid>
-      {/* Metric Three */}
-      <Grid item xl={3} lg={3} sm={6} xs={12}>
-        <MetricThree />
-      </Grid>
-      {/* Metric Four */}
-      <Grid item xl={3} lg={3} sm={6} xs={12}>
-        <MetricFour />
-      </Grid>
-    </Grid>
-  </Container>
+    <Container maxWidth={false}>
+        {/* Metric Cards */}
+        <Grid container spacing={3}>
+            {/* Metric One */}
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+                <MetricOne />
+            </Grid>
+            {/* Metric Two */}
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+                <MetricTwo />
+            </Grid>
+            {/* Metric Three */}
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+                <MetricThree />
+            </Grid>
+            {/* Metric Four */}
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+                <MetricFour />
+            </Grid>
+        </Grid>
+    </Container>
 );
