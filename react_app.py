6a6855bc7baaa6b5651bcab0157f623dd2684ffd
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-02-06 08:53:01 -0500

    clean

--- react_app.py
+++ react_app.py
@@ -7,27 +7,21 @@ from lightning.app.frontend import StaticWebFrontend
 class ReactUI(LightningFlow):
     def __init__(self):
         super().__init__()
-        # self.message_to_print = "Hello World!"
-        # self.should_print = False
 
     def configure_layout(self):
         return StaticWebFrontend(Path(__file__).parent / "reactui/dist")
 
 
-class HelloWorld(LightningFlow):
+class RootFlow(LightningFlow):
     def __init__(self):
         super().__init__()
-        self.counter = 0
         self.react_ui = ReactUI()
 
     def run(self):
         self.react_ui.run()
-        # if self.react_ui.should_print:
-        # print(f"{self.counter}: {self.react_ui.message_to_print}")
-        # self.counter += 1
 
     def configure_layout(self):
         return [{"name": "ReactUI", "content": self.react_ui}]
 
 
-app = LightningApp(HelloWorld())
+app = LightningApp(RootFlow())
