525502553752de6d93f28c575139943af679f9e7
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-02-22 08:06:36 -0500

    update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import lightning as L
 class RootFlow(L.LightningFlow):
     def __init__(self):
         super().__init__()
-        # ######################################## #
+        # ############################################ #
         # initialize your workers and child flows here #
-        # ######################################## #
+        # ############################################ #
 
     def run(self):
         # ##################################### #
